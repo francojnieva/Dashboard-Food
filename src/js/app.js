9411cef80cb6e831f12b5a1e3baf2d87ad214642
francojnieva <franco97nieva@hotmail.com>
2023-11-07 21:27:18 -0300

    :hammer: add functionality to the menu and balance section for small screens and change width of the cards on large screens.

--- src/js/app.js
+++ src/js/app.js
@@ -5,12 +5,33 @@ const sidebar = document.getElementById('sidebar')
 const imgOpenMenu = document.querySelector('.open-menu')
 const imgcloseMenu = document.querySelector('.close-menu')
 
+
 btnToggleMenu.addEventListener('click', () => {
-    imgOpenMenu.classList.toggle('hidden')  
+    if (!balanceSection.classList.contains('-right-full')) {
+        balanceSection.classList.add('-right-full')
+        balanceSection.classList.remove('right-0')
+    }
+
+    imgOpenMenu.classList.toggle('hidden')
     imgcloseMenu.classList.toggle('hidden')
     sidebar.classList.toggle('-left-0')
     sidebar.classList.toggle('-left-full')
 })
 
+// BALANCE SECTION MOBILE 
 
+const balanceSection = document.getElementById('balance-section')
+const btnToggleBalance = document.querySelector('.btnToggleBalance')
+
+btnToggleBalance.addEventListener('click', () => {
+    if (!sidebar.classList.contains('-left-full')) {
+        sidebar.classList.add('-left-full')
+        sidebar.classList.remove('-left-0')
+        imgOpenMenu.classList.remove('hidden')
+        imgcloseMenu.classList.add('hidden')
+    }
+
+    balanceSection.classList.toggle('right-0')
+    balanceSection.classList.toggle('-right-full')
+})
 
